587fbbd98577f4149fefa6d22eaaac14fc625f27
Toaster_Machine <jeremy.villanova@orange.fr>
2023-11-30 17:28:39 +0100

    Add list_articles & article

--- projet/polar_bear/models.py
+++ projet/polar_bear/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(null=True)
+    author = models.CharField(max_length=42)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+    image = models.ImageField(upload_to='polar_bear/static/polar_bear/img/', null=True, blank=True) 
+    # comment = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True)
--- projet/polar_bear/views.py
+++ projet/polar_bear/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Article
+
+def list_article(request):
+    articles = Article.objects.all()
+    return render(request, 'polar_bear/list_articles.html', {'articles': articles, 'title': 'Liste des articles'})
+
+
+def article(request, id):
+    article = get_object_or_404(Article, id=id)
+    print(article.title)
+    print(article.image)
+    return render(request, 'polar_bear/article.html', {'article': article})
