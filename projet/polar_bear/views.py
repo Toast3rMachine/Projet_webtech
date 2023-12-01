7798656696e06d6a3d6125605f3377e1a36e5833
Toaster_Machine <jeremy.villanova@orange.fr>
2023-12-01 10:20:36 +0100

    Add Search Engine

--- projet/polar_bear/views.py
+++ projet/polar_bear/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.forms import Form, CharField
 
 from .models import Article
 
+class search_article(Form):
+    search = CharField(max_length=100, required=False)
+
 def list_article(request):
     articles = Article.objects.all()
-    return render(request, 'polar_bear/list_articles.html', {'articles': articles, 'title': 'Liste des articles'})
+
+    if request.method == 'POST':
+        form = search_article(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            articles = Article.objects.filter(title__contains=search)
+    else:
+        form = search_article()
+        tile_form = request.GET.get('title', '')
+        if tile_form != '':
+            articles = Article.objects.filter(title__contains=tile_form)
+            form.fields['title'].initial = tile_form
+
+    return render(request, 'polar_bear/list_articles.html', {'search_form': form,'articles': articles, 'title': 'Liste des articles'})
 
 
 def article(request, id):
@@ -13,3 +30,4 @@ def article(request, id):
     print(article.title)
     print(article.image)
     return render(request, 'polar_bear/article.html', {'article': article})
+
